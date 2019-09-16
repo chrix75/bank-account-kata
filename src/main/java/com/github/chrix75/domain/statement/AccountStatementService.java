9d2dcedd6dd87d71e7391d1a5145a127cfd20bec
Christian Sperandio <christian.sperandio@gmail.com>
2019-09-16 12:35:32 +0200

    Typo

--- src/main/java/com/github/chrix75/domain/statement/AccountStatementService.java
+++ src/main/java/com/github/chrix75/domain/statement/AccountStatementService.java
@@ -5,7 +5,7 @@ import com.github.chrix75.domain.account.AccountNumber;
 /**
  * Interface du service charagé de produire le relevé bancaire d'un compte.
  * <p>
- * La paramétrisation est mis en place pour pouvoir gérer différents formats pour l'impression du relevé bancaire.
+ * La paramétrisation est mise en place pour pouvoir gérer différents formats pour l'impression du relevé bancaire.
  *
  * @param <T> Type de lignes du relevé bancaire
  */
